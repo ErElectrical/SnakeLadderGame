9b37efe6552d2137cde4d4f45af03154e9d35398
Mohit <94623283+ErElectrical@users.noreply.github.com>
2022-02-21 11:31:31 +0000

    Added : Welcome to Snake Ladder Game

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace snakeLadderGame
+{
+    public class program
+    {
+        public   static void Main(string[] args)
+        {
+            Console.WriteLine("welcome To SnakeLadder Game ");
+        }
+      
+    }
+        
+
+        
+
+}

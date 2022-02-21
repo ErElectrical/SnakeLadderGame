5c33561b25c41b49247ee5aec573b88f34fa0510
Mohit <94623283+ErElectrical@users.noreply.github.com>
2022-02-21 13:10:29 +0000

    Added : Feature to Play Game with single Player

--- SnakeLadderLogic.cs
+++ SnakeLadderLogic.cs
@@ -4,17 +4,75 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace snakevsLadder
+namespace snakeLadderGame
 {
     public class SnakeLadderLogic
     {
-        int position = 0;
+        public int position = 0;
+        public static bool PlayerOneFlag = true;
 
+        /// <summary>
+        /// Method give us the random number every time we roll the dice
+        /// </summary>
+        /// <returns></returns>
         private int getrolldievalue()
         {
             Random random = new Random();
             int dievalue = random.Next(1, 6);
             return dievalue;
         }
+
+        /// <summary>
+        /// method check in game of player
+        /// </summary>
+        public void PlayerCheckOption()
+        {
+            int countRoll = 0;
+
+            while (PlayerOneFlag != false)
+            {
+               
+
+                Random rem = new Random();
+                int option = rem.Next(0, 2);
+                countRoll++;
+                if (option == 0)
+                {
+                    Console.WriteLine("No Play");
+                    position += 0;
+                }
+                else if(option == 1)
+                {
+                    Console.WriteLine("Ladder Achieve ");
+                    int value = getrolldievalue();
+                    position += value;
+                }
+                else
+                {
+                    Console.WriteLine("Snake bite");
+                    int value = getrolldievalue();
+                    position -= value;
+                }
+                if(position < 0)
+                {
+                    position = 0;
+                }
+                if(position == 100 || position > 100)
+                {
+                    PlayerOneFlag = false;
+                    break;
+                }
+            }
+            PlayerOneGameAnnouncment(countRoll);
+        }
+
+        private void PlayerOneGameAnnouncment(int chances)
+        {
+            if(PlayerOneFlag == false)
+            {
+                Console.WriteLine($" you won in { chances } chances");
+            }
+        }
+                    
     }
 }
